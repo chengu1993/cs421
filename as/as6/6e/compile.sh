0e2504650f6193292b26a20e213ffad2e1e33d44
Chen Gu <chengu@Chens-MBP.wireless.yale.internal>
2017-04-27 16:39:14 -0400

    as6 tar

--- as/as6/6e/compile.sh
+++ as/as6/6e/compile.sh
@@ -10,11 +10,4 @@ sml <<EOF 2> /dev/null | egrep -v '\- |val it = |val use = |Standard|\[(linking|
     Main.compile "tests/$1";
 EOF
 gcc -m32 -ggdb3 -o $2 runtime.c tests/$1.s
-# ./prog
 
-# #!/bin/bash
-# command="CM.make \"\$smlnj-tdp/back-trace.cm\"; SMLofNJ.Internals.TDP.mode := true; CM.make \"sources.cm\"; Main.compile \"$1\";"
-# echo $command;
-# sml <<< $command
-# gcc -m32 -ggdb3 -o prog runtime.c $1.s
-# ./prog
